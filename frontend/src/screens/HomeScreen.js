53a593a05f85c82b9a146c06ea77f526142e9a5e
Andres Vidoza <andresvidoza@gmail.com>
2021-03-04 19:29:32 -0500

    Fetching Products with React and connecting it to the backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+
+// They let you use state and other React features without writing a class.
 
 const HomeScreen = () => {
+  // whhat we want to call this piece of state, what we want to call the function to manipulate products
+  const [products, setProducts] = useState([]); // pass default for products - empty array in this case
+
+  // this will run as soon as the component loads  - Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Make axios request
+    const fetchProducts = async () => {
+      const res = await axios.get('api/products') // only proceed once this is resolved (promise)
+
+      setProducts(res.data) // CHANGE STATE 
+    }
+
+    fetchProducts()
+  }, []) // if these variables change then the code inside fires off in the array
+
   return (
     <>
       <h1>Latest Products</h1>
