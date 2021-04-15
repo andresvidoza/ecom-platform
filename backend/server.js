d82a6b1302259efc2bdf9a70f713863add0096d4
Andres Vidoza <andresvidoza@gmail.com>
2021-04-15 15:11:48 -0400

    Implementing user authentication and authorization with JWT

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,12 +12,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) =>{
     res.send('API is rtunning');
 });
 
 // PRODUCT ROUTES
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // ERROR MIDDLEWARE
 app.use(notFound);
